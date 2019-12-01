1ea2e8f345dabe9e7db6d5b574fd30cc11efa7cf
Tim Weiss <tdweiss1978@gmail.com>
2019-12-01 13:29:14 -0800

    Upgrade Account Complete

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,5 +10,11 @@ router.get("/users/sign_in", userController.signInForm);
 router.post("/users/sign_in", validation.validateUserSignIn, userController.signIn);
 
 router.get("/users/sign_out", userController.signOut);
+router.get("/users/:id", userController.show);
+router.get("/users/:id/upgrade", userController.upgradePage);
+router.post("/users/:id/upgrade", userController.upgraded);
+
+router.get("/users/:id/downgrade", userController.downgrade);
+router.post("/users/:id/downgrade", userController.downgraded);
 
 module.exports = router;
