252fd6e46d2a99a39150c985d526eaf3b6a93a04
Tim Weiss <tdweiss1978@gmail.com>
2019-12-03 17:53:21 -0800

    Resubmit

--- src/db/seeds/20191127070647-user.js
+++ src/db/seeds/20191127070647-user.js
@@ -1,40 +1,25 @@
 'use strict';
 const faker = require("faker");
 
-// let users = [
-//     {
-//       name: "Joe Shmos",
-//       email: "shmos@email.com",
-//       password: "abcdef",
-//       role: "standard",
-//       createdAt: new Date(),
-//       updatedAt: new Date()
-//    },
-//    {
-//       name: "John Does",
-//       email: "does@email.com",
-//       password: "123456",
-//       role: "standard",
-//       createdAt: new Date(),
-//       updatedAt: new Date()
-//    }
-//  ];
+let users = [
+    {
+      name: "Joe Shmos",
+      email: "shmos@email.com",
+      password: "abcdef",
+      role: "standard",
+      createdAt: new Date(),
+      updatedAt: new Date()
+   },
+   {
+      name: "John Does",
+      email: "does@email.com",
+      password: "123456",
+      role: "standard",
+      createdAt: new Date(),
+      updatedAt: new Date()
+   }
+ ];
 
-
-let users = [];
-let num = 0;
-
- for(let i = 1 ; i <= 8 ; i++){
-   num = num + 1
-   users.push({
-     id: num,
-     name: faker.hacker.noun(),
-     email: faker.internet.email(),
-     password: faker.internet.password(),
-     createdAt: new Date(),
-     updatedAt: new Date()
-   });
- }
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
